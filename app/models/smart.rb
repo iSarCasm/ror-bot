a5ce02bc47528faf8e00c158191c75096f943f8f
SYPIAC <Reks888@hotmail.com>
2017-09-30 13:10:57 +0300

    add coords to neighbours

--- app/models/smart.rb
+++ app/models/smart.rb
@@ -1,37 +1,22 @@
+tBoard = Struct.new(:y, :x, :value)
+
 module Smart
   # code here
   def self.adjacent_to x, y, board
     neighbours = []
+
     neighbours << board[y][x+1] if x+1 < board[0].length
     neighbours << board[y][x-1] if x-1 > 0
     if(y.odd?) then
-      neighbours << board[y-1][x] if y - 1 > 0
-      neighbours << board[y-1][x+1] if y -1 > 0 && x + 1 < board[0].length
-      neighbours << board[y+1][x] if y + 1 < board.length
-      neighbours << board[y+1][x+1] if x + 1 < board[0].length && y + 1 < board.length
+      neighbours << tBoard.new(y-1, x, board[y-1][x]) if y - 1 > 0
+      neighbours << tBoard.new(y-1, x+1, board[y-1][x+1]) if y -1 > 0 && x + 1 < board[0].length
+      neighbours << tBoard.new(y+1, x, board[y+1][x]) if y + 1 < board.length
+      neighbours << tBoard.new(y+1, x+1, board[y+1][x+1]) if x + 1 < board[0].length && y + 1 < board.length
     else
-      neighbours << board[y-1][x-1] if y-1 > 0 && x-1 > 0
-      neighbours << board[y-1][x] if y-1 > 0
-      neighbours << board[y+1][x-1] if x-1 > 0 && y+1 < board.length
-      neighbours << board[y+1][x] if y+1 < board.length
-    end
-    return neighbours
-  end
-
-  def self.jumps_from x, y, board
-    neighbours = []
-    #neighbours << board[y;x+1] if x+1 < board[0].length
-    #neighbours << board[y;x-1] if x-1 > 0
-    if(y.odd?) then
-      neighbours << board[y-1][x-2] if y - 1 > 0 &&  x - 2 > 0
-      neighbours << board[y-1][x+1] if y -1 > 0 && x + 1 < board[0].length
-      neighbours << board[y+1][x] if y + 1 < board.length
-      neighbours << board[y+1][x+1] if x + 1 < board[0].length && y + 1 < board.length
-    else
-      neighbours << board[y-1][x-1] if y-1 > 0 && x-1 > 0
-      neighbours << board[y-1][x] if y-1 > 0
-      neighbours << board[y+1][x-1] if x-1 > 0 && y+1 < board.length
-      neighbours << board[y+1][x] if y+1 < board.length
+      neighbours << tBoard.new(y-1, x-1, board[y-1][x-1]) if y-1 > 0 && x-1 > 0
+      neighbours << tBoard.new(y-1, x, board[y-1][x]) if y-1 > 0
+      neighbours << tBoard.new(y+1, x-1, board[y+1][x-1]) if x-1 > 0 && y+1 < board.length
+      neighbours << tBoard.new(y+1, x, board[y+1][x]) if y+1 < board.length
     end
     return neighbours
   end
