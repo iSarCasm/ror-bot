df93653d7cc8791a91c30a5c9c1e654322a863ed
SarCasm <sarcasm008@gmail.com>
2017-09-30 13:35:29 +0300

    xkekek

--- app/models/smart.rb
+++ app/models/smart.rb
@@ -14,8 +14,8 @@ module Smart
   def self.adjacent_to x, y, board
     neighbours = []
 
-    neighbours << board[y][x+1] if x+1 < board[0].length
-    neighbours << board[y][x-1] if x-1 > 0
+    neighbours << Cell.new(y, x+1, board[y][x+1]) if x+1 < board[0].length
+    neighbours << Cell.new(y, x-1, board[y][x-1]) if x-1 > 0
     if(y.odd?) then
       neighbours << Cell.new(y-1, x, board[y-1][x]) if y - 1 > 0
       neighbours << Cell.new(y-1, x+1, board[y-1][x+1]) if y -1 > 0 && x + 1 < board[0].length
