5f3e49eaccb1c7dc8229f9c47a402909a8abb296
SYPIAC <Reks888@hotmail.com>
2017-09-30 12:44:20 +0300

    adjacent-to

--- app/models/smart.rb
+++ app/models/smart.rb
@@ -17,4 +17,22 @@ module Smart
     end
     return neighbours
   end
+
+  def self.jumps_from x, y, board
+    neighbours = []
+    #neighbours << board[y;x+1] if x+1 < board[0].length
+    #neighbours << board[y;x-1] if x-1 > 0
+    if(y.odd?) then
+      neighbours << board[y-1][x-2] if y - 1 > 0 &&  x - 2 > 0
+      neighbours << board[y-1][x+1] if y -1 > 0 && x + 1 < board[0].length
+      neighbours << board[y+1][x] if y + 1 < board.length
+      neighbours << board[y+1][x+1] if x + 1 < board[0].length && y + 1 < board.length
+    else
+      neighbours << board[y-1][x-1] if y-1 > 0 && x-1 > 0
+      neighbours << board[y-1][x] if y-1 > 0
+      neighbours << board[y+1][x-1] if x-1 > 0 && y+1 < board.length
+      neighbours << board[y+1][x] if y+1 < board.length
+    end
+    return neighbours
+  end
 end
